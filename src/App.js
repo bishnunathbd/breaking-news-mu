c4ebfa46cf33b0771c2b960c5ffc5eaeedb297d0
bishnunathbd <bishnunath774@gmail.com>
2021-01-22 20:05:03 +0600

    36-7-install material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { Button } from '@material-ui/core';
+import News from './components/News/News';
 
 function App() {
   return (
     <div>
-      
+      <Button color="primary">Hello World</Button>
+      <News></News>
     </div>
   );
 }
