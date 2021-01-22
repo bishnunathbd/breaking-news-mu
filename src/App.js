429d5af17ffe988ddff4f9e7d46abb6e8f5fd381
bishnunathbd <bishnunath774@gmail.com>
2021-01-22 21:01:25 +0600

    36-8-load data & display material-ui card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import { Button } from '@material-ui/core';
 import News from './components/News/News';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=39797bf826274bfeb561df80dd97059a';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setArticles(data.articles))
+  }, [])
+
   return (
     <div>
-      <Button color="primary">Hello World</Button>
-      <News></News>
+      <h2>Headlines: {articles.length}</h2>
+      {
+        articles.map(article => <News article={article}></News>)
+      }
     </div>
   );
 }
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -14,9 +14,11 @@ const useStyles = makeStyles({
   },
 });
 
-const News = () => {
+const News = (props) => {
   const classes = useStyles();
-  
+
+  const {title, description, urlToImage} = props.article;
+
   return (
     <Card className={classes.root}>
       <CardActionArea>
@@ -24,16 +26,15 @@ const News = () => {
           component="img"
           alt="Contemplative Reptile"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
+          image={urlToImage}
           title="Contemplative Reptile"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+            {title}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
+            {description}
           </Typography>
         </CardContent>
       </CardActionArea>
