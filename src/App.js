6a7f8a51cbd242c5bd12ee17aa8eabd9a5200184
bishnunathbd <bishnunath774@gmail.com>
2021-01-29 11:33:28 +0600

    40-5.1-install material icons & added fb like btn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import News from './components/News/News';
 import { useEffect, useState } from 'react';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 
 function App() {
   const [articles, setArticles] = useState([]);
@@ -8,12 +9,22 @@ function App() {
   useEffect(() => {
     const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=39797bf826274bfeb561df80dd97059a';
     fetch(url)
-    .then(res => res.json())
-    .then(data => setArticles(data.articles))
+      .then(res => res.json())
+      .then(data => setArticles(data.articles))
   }, [])
+
+  const [likeBtn, setLikeBtn] = useState('');
+
+  const handleLike = () => {
+    const color = likeBtn ? '' : 'primary';
+    setLikeBtn(color);
+  }
 
   return (
     <div>
+      <h2>Facebook Like Button</h2>
+      <ThumbUpAltIcon onClick={handleLike} color={likeBtn}></ThumbUpAltIcon>
+
       <h2>Headlines: {articles.length}</h2>
       {
         articles.map(article => <News article={article}></News>)
