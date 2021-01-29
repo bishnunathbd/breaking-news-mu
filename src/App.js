7e03111d0282507e6ed274dbc00f0df071cf548c
bishnunathbd <bishnunath774@gmail.com>
2021-01-29 14:04:56 +0600

    40-5.2-handle multiple data loading style & nested data access

--- src/App.js
+++ src/App.js
@@ -5,15 +5,33 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 
 function App() {
   const [articles, setArticles] = useState([]);
+  const [likeBtn, setLikeBtn] = useState('');
+  const [users, setUsers] = useState([]);
+  const [singleUser, setSingleUser] = useState({});
+  const [randomUser, setRandomUser] = useState({});
 
   useEffect(() => {
     const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=39797bf826274bfeb561df80dd97059a';
     fetch(url)
       .then(res => res.json())
       .then(data => setArticles(data.articles))
+
+    // users
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+
+    //single user
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.json())
+    .then(data => setSingleUser(data))
+
+    // random user
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => setRandomUser(data.results[0]))
   }, [])
 
-  const [likeBtn, setLikeBtn] = useState('');
 
   const handleLike = () => {
     const color = likeBtn ? '' : 'primary';
@@ -25,6 +43,17 @@ function App() {
       <h2>Facebook Like Button</h2>
       <ThumbUpAltIcon onClick={handleLike} color={likeBtn}></ThumbUpAltIcon>
 
+      <h3>Single User - Name: {singleUser.name} </h3>
+      <br/>
+      <h3>Random User - Gender: {randomUser.gender}</h3>
+      {/* <p>Random User - Name: {randomUser.name.first}</p>  */}
+      <p>Random User - Name: {randomUser.name && randomUser.name.first}</p>
+      <br/>
+      <h3>Total Users: {users.length}</h3>
+      {
+        users.map(user => <li>{user.name}</li>)
+      }
+      <br/>
       <h2>Headlines: {articles.length}</h2>
       {
         articles.map(article => <News article={article}></News>)
