d00dbd2cfa8f828f7143d6d10d6a9e39d517b022
bishnunathbd <bishnunath774@gmail.com>
2021-04-11 11:37:25 +0600

    40-5-1-install material icon & implement facebook like button

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 
 const useStyles = makeStyles({
   root: {
@@ -19,6 +20,15 @@ const useStyles = makeStyles({
 
 const News = (props) => {
   const {title, description, urlToImage} = props.article;
+
+  const [likeIcon, setLikeIcon] = useState('');
+
+  // const handleLikeIcon = () => {
+  //   // const color = likeIcon ? '' : 'primary';
+  //   // setLikeIcon(color);
+  //   // setLikeIcon(likeIcon ? '' : 'primary');
+  // }
+
   const classes = useStyles();
 
   return (
@@ -45,6 +55,7 @@ const News = (props) => {
         <Button size="small" color="primary">
           Learn More
         </Button>
+        <ThumbUpAltIcon onClick={() => setLikeIcon(likeIcon ? '' : 'primary')} color={likeIcon}></ThumbUpAltIcon>
       </CardActions>
     </Card>
   );
